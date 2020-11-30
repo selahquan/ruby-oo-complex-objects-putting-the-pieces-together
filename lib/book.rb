72b979a4f3b9035d7ff91e2c9a975e88f33a47dc
selahquan <josh.handon@gmail.com>
2020-11-30 16:11:38 -0500

    commit tree 283736d00fbcce14eb63b7171b83ea18e8c254af

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,55 @@
+class Book
+    def initialize(title)
+        @title = title
+    end
+    
+    def title
+        @title
+    end
 
+    def author=(author)
+        @author = author
+    end
+
+    def author
+        @author
+    end
+
+    def page_count=(num)
+        @page_count = num
+      end
+    
+      def page_count
+        @page_count
+      end
+
+      def genre=(genre)
+        @genre = genre
+      end
+    
+      def genre
+        @genre
+      end
+
+      def turn_page
+        puts "Flipping the page...wow, you read fast!"
+      end
+
+end
+
+class Shoe
+    def initialize(brand)
+        @brand= brand
+    end
+    def brand
+        @brand
+    end
+
+    attr_accessor :name, :color, :size, :material, :condition
+
+    def cobble
+        self.condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
 
